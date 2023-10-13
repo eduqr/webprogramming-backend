ceb69ca8266454949b214bf5a7e30ea2a4161e7d
eduqr <simplyy017@gmail.com>
2023-10-12 19:47:07 -0500

    Actividad 1

---
+++ web-24BM/web-24BM/Controllers/ContactoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+
+namespace web_24BM.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnviarEmail(string email, string comentario)
+        {
+            TempData["Email"] = email;
+            TempData["Comentario"] = comentario;
+            EnviarEmailSmtp(email);
+            return View("Index", "Contacto");
+        }
+
+        public bool EnviarEmailSmtp(string email)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient("mail.shapp.mx", 587);
+
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("moises.puc@shapp.mx", "Dhaserck_999");
+            mail.From = new MailAddress("moises.puc@shapp.mx", "Administrador");
+            mail.To.Add(email);
+            mail.IsBodyHtml = true;
+            mail.Body = $"Se ha contactado la persona con el correo {email} para solicitar informacion";
+            smtp.Send(mail);
+            return true;
+        }
+    }
+}
